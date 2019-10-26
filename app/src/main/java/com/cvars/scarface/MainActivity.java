3f2a447b63691c0ba96d3c79bee61cbc9daec6d5
cuppajoeman <callum.cassidynolan@mail.utoronto.ca>
2019-10-26 14:53:07 -0400

    add login method for button

--- app/src/main/java/com/cvars/scarface/MainActivity.java
+++ app/src/main/java/com/cvars/scarface/MainActivity.java
@@ -3,6 +3,9 @@ package com.cvars.scarface;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void login(View view) {
+        EditText usernameText = (EditText) findViewById(R.id.username);
+        EditText passwordText = (EditText) findViewById(R.id.password);
+        // Do logic here
+        String username = usernameText.getText().toString();
+        String password = passwordText.getText().toString();
+    }
+
+    private void login() {
+
     }
 }

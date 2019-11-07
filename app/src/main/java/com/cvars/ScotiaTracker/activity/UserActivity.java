564e71b38f66900fcdaefce4a448e2d306aa46ef
Alexander Yang <a.yang7005@gmail.com>
2019-11-07 16:43:28 -0500

    Restructured the front-end UI into fragments

---
+++ app/src/main/java/com/cvars/ScotiaTracker/activity/UserActivity.java
@@ -0,0 +1,17 @@
+package com.cvars.ScotiaTracker.activity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.cvars.ScotiaTracker.R;
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+    }
+}
---
+++ app/src/main/java/com/cvars/ScotiaTracker/fragment/TabFragment.java
@@ -0,0 +1,31 @@
+package com.cvars.ScotiaTracker.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.cvars.ScotiaTracker.R;
+import com.google.android.material.tabs.TabLayout;
+
+public class TabFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_tab, container, true);
+        TabLayout tab = view.findViewById(R.id.tab);
+
+        int[] widgets = {R.drawable.ic_home, R.drawable.ic_clipboard_notes, R.drawable.ic_search_alt, R.drawable.ic_cog};
+
+        for (int i = 0; i < tab.getTabCount(); i++) {
+            tab.getTabAt(i).setIcon(widgets[i]);
+        }
+
+        return view;
+    }
+}

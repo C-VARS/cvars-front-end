0276491b2ff32b280ba8226bc7819e03a0fc24be
rkash <raag.kashyap@mail.utoronto.ca>
2019-11-07 13:11:44 -0500

    Converting tab options into images instead.

--- app/src/main/java/com/cvars/ScotiaTracker/activity/HomeActivity.java
+++ app/src/main/java/com/cvars/ScotiaTracker/activity/HomeActivity.java
@@ -5,12 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.cvars.ScotiaTracker.R;
+import com.google.android.material.tabs.TabLayout;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private TabLayout menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        this.menu = findViewById(R.id.menu);
+        int[] widgets= {R.mipmap.scotialogo_round, R.drawable.scotialogo_background, R.drawable.scotialogo_background, R.drawable.scotialogo_background};
+
+        for (int i = 0; i<this.menu.getTabCount(); i++){
+            this.menu.getTabAt(i).setIcon(widgets[i]);
+        }
     }
 }
